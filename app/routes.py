ff6e4e0e2386c33138098d097ac4e858803e41ac
Sanjay Ramassery Babu <sanjaybabu618@gmail.com>
2022-04-09 10:00:32 +0530

    Added home screen for api reference

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return "Hi Sanjay"
+    return render_template('home.html')
 
 from app.user.routes import user
 
